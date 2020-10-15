88792026c162279c914188cc6f39d8c723195e9f
tsioutsn <61215311+tsioutsn@users.noreply.github.com>
2020-10-15 18:43:09 -0500

    Repo switch/Dev Success

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,7 +24,7 @@ class App extends React.Component {
     return (
     <div className="App">
       <header className="App-header">
-          GoodThings!
+          Kitty Condo!
       </header>
     </div>
   );
